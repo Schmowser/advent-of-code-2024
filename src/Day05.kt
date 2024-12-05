0deeedd4fe52c103b526ca61752d3d6bfc06c6d1
Schmowser <schmowser@posteo.de>
2024-12-05 08:18:17 +0100

    Solution Day06b

--- src/Day05.kt
+++ src/Day05.kt
@@ -28,18 +28,70 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        var result = 0
+        val ruleSet = mutableSetOf<Pair<String, String>>()
+        var incorrectOrderedPagesWithError = mutableSetOf<Pair<List<String>, Int>>()
 
-        return result
+        input.forEach { line ->
+            if (line.contains("|")) {
+                val rule = line.split("|")
+                ruleSet.add(rule[0] to rule[1])
+            } else if (line.contains(",")) {
+                val page = line.split(",")
+                for (i in page.indices) {
+                    for (j in i + 1 until page.size) {
+                        if (page[i] to page[j] !in ruleSet) {
+                            incorrectOrderedPagesWithError.add(page to j)
+                            return@forEach
+                        }
+                    }
+                }
+            }
+        }
+
+        println(incorrectOrderedPagesWithError)
+        val fixedPages = mutableSetOf<List<String>>()
+
+        while (incorrectOrderedPagesWithError.isNotEmpty()) {
+            val newPages = incorrectOrderedPagesWithError.map {
+                val newPage = it.first.toMutableList()
+                (newPage[it.second - 1] to newPage[it.second]).apply {
+                    newPage[it.second - 1] = second
+                    newPage[it.second] = first
+                }
+                newPage
+            }
+
+            println(newPages)
+            incorrectOrderedPagesWithError.clear()
+
+            newPages.forEach { page ->
+                for (i in page.indices) {
+                    for (j in i + 1 until page.size) {
+                        if (page[i] to page[j] !in ruleSet) {
+                            incorrectOrderedPagesWithError.add(page to j)
+                            return@forEach
+                        }
+                    }
+                }
+                fixedPages.add(page)
+            }
+
+            println(incorrectOrderedPagesWithError)
+        }
+
+        println(fixedPages)
+        return fixedPages.sumOf { page ->
+            page[page.size / 2].toInt()
+        }
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day05_test")
-    part1(testInput).println()
-    part2(testInput).println()
+    //part1(testInput).println()
+    //part2(testInput).println()
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day05")
-    part1(input).println()
+    //part1(input).println()
     part2(input).println()
 }
